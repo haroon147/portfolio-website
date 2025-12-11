afba01aaf8b0e305d2ac37744d8580db842a7dc6
Mohammad Haroon <haroon147@gmail.com>
2025-12-11 12:40:43 +0500

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Change this to '/your-repo-name/' if deploying to a subdirectory
+  base: '/portfolio-website/',
 })
